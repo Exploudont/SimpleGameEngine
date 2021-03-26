e971bbe7fabd8b8e88cdbbee8a113e6c86757dd7
exploudont <daniele.longobardi@libero.it>
2021-03-26 13:45:14 +0100

    Added Test for LoggingSystem

---
+++ src/loggingsystem/test/test_log.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../Logger.h"
+
+int main(int argc, const char* argv[]) {
+
+	const char* msg = "test";
+	
+	LOG_INFO(msg);
+	LOG_WARNING(msg);
+	LOG_ERROR(msg);
+	LOG_FATAL(msg);
+	LOG_DEBUG(msg);
+	
+	return 0;
+}
